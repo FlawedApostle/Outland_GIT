f8a0d70adce8ec00edfef79727cf63c49145c808
FlawedApostle <smafearnley@gmail.com>
2025-12-20 15:44:48 -0500

    TorsoRotate , New TorsoBone script , new environment test , new prefabs

--- Assets/Scripts/Controls/TorsoMovement.cs
+++ Assets/Scripts/Controls/TorsoMovement.cs
@@ -9,13 +9,19 @@ public class TorsoMovement : MonoBehaviour
     private float _rotationX = 0f;
     private float _rotationY = 0f;
 
+    float cameraYaw = 0f;
+    float torsoYaw = 0f;
+
     private void LateUpdate()
     {
         //1. we need the Camera yaw !
         /// We are taking the Main Scene Camera - which has a tag NAMED "MAIN CAMERA" (WHICH IS UNITYS DEFAULT CAMERA TAG NAME) 
-        float cameraYaw = Camera.main.transform.eulerAngles.y;
+        cameraYaw = Camera.main.transform.eulerAngles.y;
+        Debug.Log("cameraYaw eulerAngle.y : " +  cameraYaw);
         //2. We need the torso yaw
-        float torsoYaw = torsoBone.eulerAngles.y;
+        torsoYaw = torsoBone.eulerAngles.y;
+        Debug.Log("torsoYaw eulerAngle.y : " + torsoYaw);
+
 
     }
 
