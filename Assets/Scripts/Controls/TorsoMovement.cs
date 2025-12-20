93db4bcf72cbce0b4490498fd9c0f253f2540c4b
FlawedApostle <smafearnley@gmail.com>
2025-12-20 16:05:48 -0500

    TorsoRotate , New TorsoBone script

--- Assets/Scripts/Controls/TorsoMovement.cs
+++ Assets/Scripts/Controls/TorsoMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class TorsoMovement : MonoBehaviour
 {
     [SerializeField] Transform torsoBone; // Drag the head bone here in the Inspector
+    [SerializeField] Transform headBoneYaw; // get the direction of the headBone - ONLY the yaw 
     [SerializeField] float sensitivity = 2.0f;
     [SerializeField] float verticalLimit = 60f;
 
@@ -11,6 +12,9 @@ public class TorsoMovement : MonoBehaviour
 
     float cameraYaw = 0f;
     float torsoYaw = 0f;
+    float headYaw = 0f;
+
+    float rotateTimer = 0f;
 
     private void LateUpdate()
     {
@@ -21,7 +25,15 @@ public class TorsoMovement : MonoBehaviour
         //2. We need the torso yaw
         torsoYaw = torsoBone.eulerAngles.y;
         Debug.Log("torsoYaw eulerAngle.y : " + torsoYaw);
+        // 3. we need the head yaw
+        headYaw = headBoneYaw.eulerAngles.y;
+        Debug.Log("headYaw eulerAngle.y : " + headYaw);
+        // 4. we need relative camera 'movememt' ?
+        // do i need to get relative movement ? I dont think so no.
 
+        // if the head yaw is facing a direction for a period of time 
+        // then rotate the body yaw to face the head direction
+        
 
     }
 
