7ef35dac27b477f1df70a61efa4e13bac8cb74aa
FlawedApostle <smafearnley@gmail.com>
2026-01-06 13:55:02 -0500

    rotatebody script edit

--- Assets/Scripts/Game/Debugger.cs
+++ Assets/Scripts/Game/Debugger.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Debuger : MonoBehaviour
 {
-    [Header("TorsoBone Script Reference")]
+    [Header("SCRIPTS: Rotate Body Reference")]
     [SerializeField]// Tooltip("Reference to the RelativeMovement script for movement direction")]
     private RotateBodyMovement DEBUG_RotateBodyMovement;
 
-    [Header("Relative Movement Script Reference")]
+    [Header("SCRIPTS: Relative Movement Reference")]
     [SerializeField, Tooltip("Reference to the RelativeMovement script for movement direction")]
-    private RelativeMovement DEBUG_relativeMovement;
+    private RelativeMovement DEBUG_RelativeMovement;
 
-    [Header("Animator Script Reference")]
+    [Header("SCRIPTS: Head Bone Movement Reference")]
+    [SerializeField, Tooltip("Reference to the RelativeMovement script for movement direction")]
+    private HeadBoneMovement DEBUG_HeadBoneMovement;
+
+    [Header("SCRIPTS: Animator Script Reference")]
     //[SerializeField, Tooltip("Reference to the RelativeMovement script for movement direction")]
     private Animator DEBUG_animator;
 
@@ -18,7 +22,8 @@ public class Debuger : MonoBehaviour
     private void Update()
     {
 
-        Debug.Log("MoveDir: " + DEBUG_RotateBodyMovement.Get_moveDir() + " | targetYaw: " + DEBUG_RotateBodyMovement.Get_targetYaw());
+        //Debug.Log("HeadBone Rotation Direction: " + DEBUG_HeadBoneMovement.Get_HeadBone_Rotation() + " | HeadBone Rotation Yaw: " + DEBUG_HeadBoneMovement.Get_HeadBone_Yaw());
+        //Debug.Log("MoveDir: " + DEBUG_RotateBodyMovement.Get_moveDir() + " | targetYaw: " + DEBUG_RotateBodyMovement.Get_targetYaw());
     }
 
 
