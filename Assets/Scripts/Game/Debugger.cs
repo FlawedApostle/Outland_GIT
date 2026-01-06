feb46009579140d27eb903d19f37dbbed74fcf7f
FlawedApostle <smafearnley@gmail.com>
2026-01-06 14:16:42 -0500

    debugger fixed - added HeadBone Movement

--- Assets/Scripts/Game/Debugger.cs
+++ Assets/Scripts/Game/Debugger.cs
@@ -13,20 +13,36 @@ public class Debuger : MonoBehaviour
     [Header("SCRIPTS: Head Bone Movement Reference")]
     [SerializeField, Tooltip("Reference to the RelativeMovement script for movement direction")]
     private HeadBoneMovement DEBUG_HeadBoneMovement;
+    public float NULLCHECK_HeadBoneMovement()      // DEBUG
+    {
+        if (DEBUG_HeadBoneMovement == null) { Debug.LogError("DEBUG_HeadBoneMovement is NULL!"); }
+        return 0f;
+    }
 
     [Header("SCRIPTS: Animator Script Reference")]
     //[SerializeField, Tooltip("Reference to the RelativeMovement script for movement direction")]
     private Animator DEBUG_animator;
 
 
+    private void Start()
+    {
+        NULLCHECK_HeadBoneMovement();
+    }
     private void Update()
     {
+        
+        if (Input.GetKeyDown(KeyCode.F9))
+        {
+            //Debug.Log("F9 pressed!");
+            Debug.Log("HeadBone Rotation Direction: " + DEBUG_HeadBoneMovement.Get_HeadBone_Rotation() + " | HeadBone Rotation Yaw: " + DEBUG_HeadBoneMovement.Get_HeadBone_Yaw());
 
-        //Debug.Log("HeadBone Rotation Direction: " + DEBUG_HeadBoneMovement.Get_HeadBone_Rotation() + " | HeadBone Rotation Yaw: " + DEBUG_HeadBoneMovement.Get_HeadBone_Yaw());
+        }
+
+           
         //Debug.Log("MoveDir: " + DEBUG_RotateBodyMovement.Get_moveDir() + " | targetYaw: " + DEBUG_RotateBodyMovement.Get_targetYaw());
+        //Debug.Log("isWalking = " +  DEBUG_relativeMovement.Get_statusAnimator()); //+ " | magnitude = " + inputMovementMagnitude);       /// DEBUG
     }
 
 
-    //Debug.Log("isWalking = " +  DEBUG_relativeMovement.Get_statusAnimator()); //+ " | magnitude = " + inputMovementMagnitude);       /// DEBUG
 
 }
