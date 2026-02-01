5bff3d9b12eab40180ce874f4b5e5dab135144af
FlawedApostle <smafearnley@gmail.com>
2026-02-01 00:56:13 -0500

    Added new exported walls, floors regular and floor and inner corners are working end has not been re-exported [ensure that is done] inner build is currently in use inner, and outer builds work. - minus end wall and large corners whom are not exported yet. all items have been tested for width length and height. -matching. inner and outer. flickering lights script enemy added enemy ai - patrol , enemy chase added new world bulding with newly added pre fabs enemy animations idle, walk and run on spot all working, and set inside enemy animation controller

---
+++ Assets/Scripts/Lights/LightFlicker.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    private Light localLight;
+    public float minIntensity = 0.2f; // Dimness
+    public float maxIntensity = 1.2f; // Brightness
+    public float flickerSpeed = 0.05f; // How fast it stutters
+
+    void Start()
+    {
+        localLight = GetComponent<Light>();
+        // Start the flickering loop
+        InvokeRepeating("Flicker", 0, flickerSpeed);
+    }
+
+    void Flicker()
+    {
+        // Pick a random brightness between our min and max
+        localLight.intensity = Random.Range(minIntensity, maxIntensity);
+    }
+}
