6cdc72f43b683b50c215c031b958a86c5b3f2167
FlawedApostle <smafearnley@gmail.com>
2026-03-08 17:38:39 -0400

    Ai stalker script opening doors

--- Assets/Scripts/Ai/StalkerAI.cs
+++ Assets/Scripts/Ai/StalkerAI.cs
@@ -20,25 +20,42 @@ public class StalkerAI : MonoBehaviour
 
     void Update()
     {
-        // NEW: direct Follow Mode
+        //error check
+        if (!agent.isOnNavMesh || !agent.isActiveAndEnabled) return;
+        
+        // Direct Follow Mode
         if (directFollowMode && followTarget != null)
         {
             agent.SetDestination(followTarget.position);
             return;
         }
+        
         stalkTimer += Time.deltaTime;
-
         // Every 5 seconds, move to a "hiding spot" near the player
         // need to add - a navmesh radius/space checker ? (Random.insideUnitSphere * 10f); - float value 10 should be the navmesh radius / then we can implement 'hot-zones' - randomSpotInZone
+        Vector3 dir = (player.position - transform.position).normalized;
+        Vector3 target = player.position - dir * 3f;
         if (stalkTimer > 5f)
         {
-            Vector3 randomSpotNearPlayer = player.position + (Random.insideUnitSphere * 10f);
             NavMeshHit hit;
-            if (NavMesh.SamplePosition(randomSpotNearPlayer, out hit, 10f, NavMesh.AllAreas))
+            if (NavMesh.SamplePosition(target, out hit, 2f, NavMesh.AllAreas))
             {
-                agent.SetDestination(hit.position);
+                // to stop spamming the same point
+                if (Vector3.Distance(agent.destination, hit.position) > 1f)         //agent.SetDestination(hit.position);
+                {
+                    agent.SetDestination(hit.position);
+                }
             }
             stalkTimer = 0;
         }
     }
 }
+
+
+
+// OLD CODE
+///Vector3 randomSpotNearPlayer = player.position + (Random.insideUnitSphere * 10f);
+///if (NavMesh.SamplePosition(randomSpotNearPlayer, out hit, 10f, NavMesh.AllAreas))
+///{
+///    agent.SetDestination(hit.position);
+///}
